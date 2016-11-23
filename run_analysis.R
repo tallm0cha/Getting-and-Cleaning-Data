6d6e28b2485e4b3641450b042f770b26e537dddb
tallm0cha <gaurav.a.singh@zsassociates.com>
2016-11-23 13:03:49 -0800

    This is the first commit and contains the R code and the dataset required for the programming assignment for Coursera's Getting and Cleaning Data course

---
+++ run_analysis.R
@@ -0,0 +1,52 @@
+library(dplyr)
+library(tidyr)
+
+#Define paths
+path <- getwd()
+path <- paste(path,"/UCI HAR Dataset/",sep="")
+pathTest <- paste(path,"/test/",sep="")
+pathTrain <- paste(path,"/train/",sep="")
+
+#Load files
+## Test files
+xtest <- read.table(paste(pathTest,"/X_test.txt",sep=""),header=F)
+ytest <- read.table(paste(pathTest,"/y_test.txt",sep=""),header=F)
+subtest <- read.table(paste(pathTest,"/subject_test.txt",sep=""),header=F)
+## Train files
+xtrain <- read.table(paste(pathTrain,"/X_train.txt",sep=""),header=F)
+ytrain <- read.table(paste(pathTrain,"/y_train.txt",sep=""),header=F)
+subtrain <- read.table(paste(pathTrain,"/subject_train.txt",sep=""),header=F)
+## Features and Activity
+features <- read.table(paste(path,"features.txt",sep=""),header=F)
+activity <- read.table(paste(path,"activity_labels.txt",sep=""),header=F)
+colnames(activity) <- c("Activity.ID","Activity.Label")
+
+#Merge files
+testMerged_suby <- cbind.data.frame(subtest, ytest)
+testMerged <- cbind.data.frame(testMerged_suby, xtest)
+trainMerged_suby <- cbind.data.frame(subtrain, ytrain)
+trainMerged <- cbind.data.frame(trainMerged_suby, xtrain)
+
+mergedData <- rbind.data.frame(testMerged,trainMerged)
+
+#Add column names
+featCols <- as.character(features$V2)
+colNames <- c("Subject","Activity ID",featCols)
+vColNames <- make.names(colNames, unique=TRUE, allow_ = TRUE)
+colnames(mergedData) <- vColNames
+
+#Extract Mean and Std Dev
+meansStddev<-select(mergedData, Subject,Activity.ID,contains("mean."), contains("std."),-contains("angle"))
+
+#Add Activity labels
+Step4Data<-merge(activity,meansStddev, by.x="Activity.ID", by.y="Activity.ID")
+Step4Data<-select(Step4Data,-Activity.ID)
+
+#Generate averages
+Step4tidyData <- gather(Step4Data, Variable, Value, -Subject, -Activity.Label )
+Step5Data <- Step4tidyData %>% 
+  group_by(Activity.Label,Subject) %>%
+  summarise(Activity.Avg=mean(Value))
+
+#Write final table
+write.table(Step5Data,file="Final Tidy Dataset.txt", row.names = F)
